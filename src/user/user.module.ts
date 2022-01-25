38aa9f2725ef6986e8247e678bf3e7245499bea7
Paulo Salvatore <salvatoregames@gmail.com>
2022-01-25 11:05:31 -0300

    fix(user): import `PrismaModule` in the `UserModule`

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
