776558f1882c91158894a60466ce724824b2926c
Paulo Salvatore <salvatoregames@gmail.com>
2022-01-25 11:16:31 -0300

    feat(app): create `/me` endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CurrentUser } from './auth/decorators/current-user.decorator';
+import { User } from './user/entities/user.entity';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/me')
+  getMe(@CurrentUser() currentUser: User) {
+    return currentUser;
   }
 }
